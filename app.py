5c4a7f802e5f2c0b8c9707d213d3014c2bf38a81
Me <sksandeshkandagal@gmail.com>
2022-02-16 00:30:24 +0530

    deploymet problem

--- app.py
+++ app.py
@@ -18,67 +18,15 @@ def home():
 
 @app.route("/predict", methods=['POST'])
 def predict():
-    try:
-        if request.json is not None:
-            path = request.json['filepath']
+    pred_val = pred_validation()  # object initialization
 
-            pred_val = pred_validation(path)  # object initialization
+    pred_val.pred_validation()  # calling the prediction_validation function
 
-            pred_val.pred_validation()  # calling the prediction_validation function
+    pred = predict_data()  # object initialization
 
-            pred = predict_data(path)  # object initialization
-
-            # predicting for dataset present in database
-            path = pred.predict_data()
-            return render_template('index.html')
-        elif request.form is not None:
-            path = request.form['filepath']
-
-            pred_val = pred_validation(path)  # object initialization
-
-            pred_val.pred_validation()  # calling the prediction_validation function
-
-            pred = predict_data()  # object initialization
-
-            # predicting for dataset present in database
-            path = pred.predict_data()
-            return render_template('index.html')
-        else:
-            print('Nothing Matched')
-    except ValueError:
-        return Response("Error Occurred! %s" % ValueError)
-    except KeyError:
-        return Response("Error Occurred! %s" % KeyError)
-    except Exception as e:
-        return Response("Error Occurred! %s" % e)
-
-
-@app.route("/train", methods=['POST'])
-def trainRouteClient():
-
-    try:
-        if request.json['folderPath'] is not None:
-            path = request.json['folderPath']
-
-            train_valObj = train_validation()  # object initialization
-
-            train_valObj.train_validation()  # calling the training_validation function
-
-            trainModelObj = trainmodel()  # object initialization
-            trainModelObj.train_model()  # training the model for the files in the table
-
-    except ValueError:
-
-        return Response("Error Occurred! %s" % ValueError)
-
-    except KeyError:
-
-        return Response("Error Occurred! %s" % KeyError)
-
-    except Exception as e:
-
-        return Response("Error Occurred! %s" % e)
-    return Response("Training successfull!!")
+    # predicting for dataset present in database
+    path = pred.predict_data()
+    return render_template('index.html')
 
 
 if __name__ == "__main__":
