920afdc746a2b3432f160b45594f36605140cc83
Me <sksandeshkandagal@gmail.com>
2022-02-16 04:02:21 +0530

    deploy

--- app.py
+++ app.py
@@ -1,24 +1,33 @@
 import numpy as np
 from flask import Flask, request, jsonify, render_template
 import pickle
+from Database_operation.cassandra_data_op import cassandra_ops
+import pandas as pd
+from File_operations.file_ops import file_operations
+from train_valid import train_validation
+from train_model import trainmodel
+from pred_valid import pred_validation
+from prediction import predict_data
+
 
 app = Flask(__name__)
-model = pickle.load(open('model.pkl', 'rb'))
+
 
 @app.route('/')
 def home():
     return render_template('index.html')
 
-@app.route('/predict',methods=['POST'])
+
+@app.route('/predict', methods=['POST'])
 def predict():
     '''
     For rendering results on HTML GUI
     '''
-    int_features = [int(x) for x in request.form.values()]
-    final_features = [np.array(int_features)]
-    prediction = model.predict(final_features)
+    c = pred_validation()
+    c.pred_validation()
 
-    output = round(prediction[0], 2)
+    d = predict_data()
+    d.predict_data()
 
     return render_template('index.html', prediction_text='Employee Salary should be $ {}'.format(output))
 
